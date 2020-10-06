3143a3b26598500b1b46aed61f19a64b926e3f7d
fulopd <fulopdaniel@gmail.com>
2020-10-06 22:03:24 +0200

    server fogadott adatokat menti nedb -vel

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3999;
+const Datastore = require('nedb');
+
+const db = new Datastore({filename: 'database.db'});
+db.loadDatabase();
+
 
 app.use(express.static('public'));
 app.use(express.json({filesize: '1 mb'}));
@@ -11,6 +16,7 @@ app.listen(port, () => console.log(`Server listen on ${port} port.`));
 app.post('/', (response, request) => {
     console.log('/');
     const data = response.body;
+    db.insert(data);
     console.log(data);
     request.json({
         status: 'success',
